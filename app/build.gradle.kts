d255dc5b67472aff28de568879ac3109d57cac77
zanyarshokri1995-prog <zanyarshokri1995@gmail.com>
2026-09-11 21:44:36 +0330

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.zeriva.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.zeriva.app"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
