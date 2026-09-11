432c05f23849839507d74f6c9de0f8a97e3b1862
zanyarshokri1995-prog <zanyarshokri1995@gmail.com>
2026-09-11 22:27:42 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.6.1")
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.6.1" apply false
+    id("com.android.application") apply false
 }
